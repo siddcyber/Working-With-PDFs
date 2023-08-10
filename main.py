44a71dcc97846f2c0f8f60816abb683278283944
siddcyber <siddhantgulia@gmail.com>
2023-08-10 21:26:43 +0530

    changing code to test more efficient way with just one loop

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os
 from PIL import Image
 import PyPDF2
+from pathlib import Path
 
 src_folder = './images'
 pdf_folder = './pdf'
-filename = 'MCA Introduction to Computer & Programming Concepts'
+filename = 'Memes'
 file_List = []
 file_type_list = []
+names = []
+def image_to_pdf(file, name):
+    # converted_files = []  # New list to store converted files
+    print(file)
+    img = Image.open(file)
+    converted = img.convert('RGB')
+    # converted_files.append(converted)
+    converted.save(os.path.join(pdf_folder, name + '.pdf'))
+
 
 def convert_merge(file_type_list, file_List):
-    converted_files = []  # New list to store converted files
-
-    if file_type_list == "Image":
-        for i in file_List:
-            img = Image.open(i)
-            converted = img.convert('RGB')
-            converted_files.append(converted)
-        converted_files[0].save(os.path.join(pdf_folder, filename + '.pdf'), save_all=True,
-                                append_images=converted_files[1:])
-
-    elif file_type_list == "PDF":
+    if file_type_list == "PDF":
         pdfMerge = PyPDF2.PdfMerger()
         for i in file_List:
             pdfFile = open(i, 'rb')
@@ -35,14 +35,24 @@ def convert_merge(file_type_list, file_List):
 for file in os.listdir(src_folder):
     if file.endswith('.pdf'):
         file_List.append(os.path.join(src_folder, file))
+        names.append(Path(file).stem)
         file_type_list.append('PDF')
     elif file.endswith('.jpg') or file.endswith('jpeg') or file.endswith('png'):
         file_List.append(os.path.join(src_folder, file))
+        names.append(Path(file).stem)
         file_type_list.append('Image')
     elif file.endswith('.docx'):
         file_List.append(os.path.join(src_folder, file))
+        names.append(Path(file).stem)
         file_type_list.append('Word')
 
 print(file_type_list, file_List)
-# convert_merge(file_type_list, file_List)
+print(names)
+
+for i in range(len(file_List)):
+    if file_type_list[i] == 'Image':
+        image_to_pdf(file_List[i], names[i])
+    else:
+        pass
+
 
