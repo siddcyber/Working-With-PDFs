4960fcb27826fd94528dd290e597d09a4c7429b2
siddcyber <siddhantgulia@gmail.com>
2023-11-01 20:47:40 +0530

    changing code to test more efficient way with just one loop -> added new file to merge PDFs only -> added code to check if pixels are too large and resize them

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ from PIL import Image
 
 src_folder = './images'
 pdf_folder = './pdf'
-filename = 'Memes'
+filename = 'Deloitte_images'
 file_List = []
-
+# add max image pixels size if needed
+# Image.MAX_IMAGE_PIXELS = 108000000
 
 def convert_merge(file_List):
     converted_files = []  # New list to store converted files
 
     for i in file_List:
         img = Image.open(i)
+        w, h = img.size
+
+        if w > 1920:
+            print('Image is too large: {}:{}, aspect ratio is {}'.format(w, h, w/h))
+            ratio = w/h
+            img = img.resize((1920, int(1920/ratio)))
+            print('Image is resized to {}:{}'.format(img.size[0], img.size[1]))
+        else:
+            print('Image is good: {}:{}, aspect ratio is {}'.format(w, h, w/h))
+
         converted = img.convert('RGB')
         converted_files.append(converted)
     converted_files[0].save(os.path.join(pdf_folder, filename + '.pdf'), save_all=True,
@@ -22,5 +33,7 @@ for file in os.listdir(src_folder):
     if file.endswith('.jpg') or file.endswith('jpeg') or file.endswith('png'):
         file_List.append(os.path.join(src_folder, file))
 
+print(len(file_List))
+print(file_List)
 convert_merge(file_List)
 
